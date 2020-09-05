274ffd6cd88a507fd99df37d605c06edd8093c3a
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-09-05 15:42:19 +0300

    add User class and user_test.py

---
+++ user.py
@@ -0,0 +1,13 @@
+class User:
+    '''
+    Class that generates new Usernames and passwords
+    '''
+    pass
+
+    users_list=[]
+    def __init__(self,name,password):
+        '''
+        A function to create username and password
+        '''
+        self.name = name
+        self.password= password
---
+++ user_test.py
@@ -0,0 +1,19 @@
+import unittest
+from user import User
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for user class behaviours
+    '''
+    new_user=User("Brian","12345")
+    def SetUp(self):
+        self.new_user = User ("Brian","12345")
+
+    def test_init_(self):
+        self.assertEqual(self.new_user.name,"Brian")
+        self.assertEqual(self.new_user.password,"12345")
+
+if __name__== '__main__':
+    unittest.main()
+
+  
