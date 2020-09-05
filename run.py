868b9ff68beef4363e3bc226e6a0eaac40cdea53
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-09-05 18:06:20 +0300

    add save_user function to run.py

--- run.py
+++ run.py
@@ -7,7 +7,11 @@ def __init__(self,name,password):
     '''
     self.name = name
     self.password= password
-
+def save_user(self):
+    '''
+    A function that allows us to save users password
+    '''
+    User.users_list.append(self) 
    
    
  
