645235af975ed5eff24c8f4a12fb1b9011f55ec8
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-09-05 18:04:18 +0300

    add __init__ method to run.py

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3.6
+from user import User
+users_list=[]
+def __init__(self,name,password):
+    '''
+    A function to create username and password
+    '''
+    self.name = name
+    self.password= password
 
-
+   
+   
+ 
