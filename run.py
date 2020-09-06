2b870f4a4ac5971e3683dc9850e79593bb296277
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-09-06 14:57:17 +0300

    add a function that signs up and saves a users credentials

--- run.py
+++ run.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3.6
 from user import User
-
-        
+def sign_up(name,password):
+    '''
+    A function that allows a user to sign up
+    '''
+    new_user = User(name,password)
+    return new_user
+def save_user(user):
+    '''
+    A function that saves users credentials
+    '''
+    user.save_contact()
+def delete_user(user):
+    '''
     
