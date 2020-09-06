d4635448e9aa4648bf0fd453af6335b38cfa0f1e
Brian Tinega Nyang'wara <briantinega@gmail.com>
2020-09-06 14:44:27 +0300

    delete initial functions in run.py

--- run.py
+++ run.py
@@ -1,27 +1,5 @@
 #!/usr/bin/env python3.6
 from user import User
-users_list=[]
-def __init__(self,name,password):
-    '''
-    A function to create username and password
-    '''
-    self.name = name
-    self.password= password
-def save_user(self):
-    '''
-    A function that allows us to save users password
-    '''
-    User.users_list.append(self) 
 
-def delete_user(self):
-    '''
-    A function that deletes a users credentials
-    '''
-    User.users_list.remove(self)  
-   
-@classmethod
-def display_users(cls):
-    '''
-    A function that displays all users saved in a user list
-    '''
-    return cls.users_list
+        
+    
